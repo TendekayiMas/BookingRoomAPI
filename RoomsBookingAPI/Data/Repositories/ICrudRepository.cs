9f927117f09edf7f1f19754b3de177666be05b66
TendekayiMas <103124438+TendekayiMas@users.noreply.github.com>
2022-05-23 20:18:43 +0100

    adding ICrud in repo

---
+++ RoomsBookingAPI/Data/Repositories/ICrudRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace RoomsBookingAPI.Data.Repositories
+{
+    public interface ICrudRepository<T,U>
+    {
+        // Implementing the crud functions for interface
+        public IEnumerable<T> GetAll();
+        public T Get(U id);
+        public void Add(T element);
+        public void Update(T element);
+        public void Delete(U id);
+        public bool Exists(U id);
+        public bool Save();
+
+    }
+}
