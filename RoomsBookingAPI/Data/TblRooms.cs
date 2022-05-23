d127aacf3151d2877f9fb013fabf69372b15aebc
TendekayiMas <103124438+TendekayiMas@users.noreply.github.com>
2022-05-23 19:37:10 +0100

    adding class for tblRooms

---
+++ RoomsBookingAPI/Data/TblRooms.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomsBookingAPI.Data
+{
+    public class TblRooms
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public string? RoomType { get; set; }
+
+        public bool RoomAvailability { get; set; }
+
+        public int RoomPrice
+        {
+            get; set;
+        }
+}
