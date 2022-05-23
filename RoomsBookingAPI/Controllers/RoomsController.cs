93616db99b59275cd88074c57b3b16b0fb2dce73
TendekayiMas <103124438+TendekayiMas@users.noreply.github.com>
2022-05-23 15:29:12 +0100

    Creating room controller

---
+++ RoomsBookingAPI/Controllers/RoomsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoomsBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet("GetRooms")]
+        public IActionResult Get()
+        {
+            return Ok(); // return an ok result of 200 for now 
+        }
+        [HttpPost("CreateRoom")]
+
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("UpdateRoom")]
+
+        public IActionResult update()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("DeleteRoom")]
+
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
