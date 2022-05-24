cc4d3ef38fcf9c2804061c1dd422212479ba3e9d
TendekayiMas <103124438+TendekayiMas@users.noreply.github.com>
2022-05-24 11:40:04 +0100

    Updating program cs

--- RoomsBookingAPI/Program.cs
+++ RoomsBookingAPI/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using RoomsBookingAPI.Data;
+using RoomsBookingAPI.Data.Repositories;
+using RoomsBookingAPI.Models;
+using RoomsBookingAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<RoomsDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICrudRepository<TblRooms, int>, RoomRepository>();
+builder.Services.AddScoped<ICrudServices<TblRooms, int>, RoomServices>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RoomsBookingRestAPI",
+        Version =
+    "v1"
+    });
+});
 
 var app = builder.Build();
 
@@ -16,10 +36,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
